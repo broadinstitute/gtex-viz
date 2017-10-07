bce5b15032b1001305f7d51493f4e52629c19fa7
kataviz <khhuang@broadinstitute.org>
2017-10-07 10:56:02 -0400

    adding the color legends

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 // render the heatmap
 const containerId = "chart";
+
+// TODO: figure out the layout dimensions!
 const margin = {
     left: 160,
-    top: 150,
+    top: 200,
     right: 100,
     bottom: 200
 };
@@ -15,8 +17,8 @@ const palette = {
     blues: d3.schemeBlues[9],
     gnbu: d3.schemeGnBu[9],
     reds: ["#FFE4DE", "#FFC6BA", "#F7866E", "#d9745e", "#D25C43", "#b6442c", "#9b3a25", "#562015"],
-    ygb: ["#FFF09D", "#F0EFB4", "#E2E9A6", "#D3E397", "#BCDCA5", "#A4D4B3", "#8DCDC1", "#87B1A4","#829487", "#7C786A"]
-
+    ygb: ["#FFF09D", "#F0EFB4", "#E2E9A6", "#D3E397", "#BCDCA5", "#A4D4B3", "#8DCDC1", "#87B1A4","#829487", "#7C786A"],
+    
 };
 
 const svgConfig = {
@@ -38,6 +40,121 @@ const mapg = svg.append("g")
 const leftg = svg.append("g")
     .attr('id', 'leftgroup')
     .attr("transform", `translate(5, ${margin.top})`);
+
+const topg = svg.append("g")
+    .attr('id', 'topgroup')
+    .attr("transform", `translate(${margin.left}, 50)`);
+
+const legendg = svg.append("g")
+    .attr('id', 'legendgroup')
+    .attr("transform", `translate(${margin.left}, 0)`);
+
+// parse the gene dendrogram
+const gene_newick = "(((((SLC25A21:1.47,GAS6-AS1:1.47):1.53,SLC27A6:2.99):0.84,TMEM229A:3.83):2.34,((TMEM255B:2.47,TMEM106B:2.47):0.62,GAS6-AS2:3.08):3.09):7.25,(TMEM167B:4.53,GAS6:4.53):8.89);";
+const gene_tree = new Dendrogram(gene_newick);
+gene_tree.draw(leftg);
+
+// parse the tissue dendrogram
+const tissue_newick = "((((((((((Testis:0.68,Brain_Cerebellum:0.68):0.30,Prostate:0.98):0.17,(Brain_Nucleus_accumbens_basal_ganglia:0.78,Brain_Hypothalamus:0.78):0.36):0.31,Cervix_Ectocervix:1.45):0.35,Esophagus_Gastroesophageal_Junction:1.80):0.38,Pituitary:2.19):0.65,(((Brain_Putamen_basal_ganglia:0.83,Brain_Cortex:0.83):0.50,Skin_Not_Sun_Exposed_Suprapubic:1.32):0.51,Brain_Hippocampus:1.84):1.00):1.13,((((((Brain_Frontal_Cortex_BA9:0.47,Brain_Anterior_cingulate_cortex_BA24:0.47):0.20,Bladder:0.67):0.15,((Breast_Mammary_Tissue:0.47,Brain_Cerebellar_Hemisphere:0.47):0.15,Brain_Caudate_basal_ganglia:0.62):0.21):0.31,Esophagus_Mucosa:1.13):0.24,(Spleen:0.75,Colon_Sigmoid:0.75):0.63):0.48,(((Cells_Transformed_fibroblasts:0.26,Cells_EBV-transformed_lymphocytes:0.26):0.30,(Cervix_Endocervix:0.24,Brain_Spinal_cord_cervical_c-1:0.24):0.32):0.05,(((Brain_Substantia_nigra:0.15,Brain_Amygdala:0.15):0.09,Skin_Sun_Exposed_Lower_leg:0.25):0.24,Small_Intestine_Terminal_Ileum:0.48):0.13):1.24):2.11):2.90,(((Whole_Blood:1.14,Stomach:1.14):0.75,((((Uterus:0.66,Heart_Left_Ventricle:0.66):0.05,Kidney_Cortex:0.71):0.32,((Liver:0.38,Heart_Atrial_Appendage:0.38):0.19,Fallopian_Tube:0.57):0.45):0.06,Vagina:1.08):0.82):0.62,((Esophagus_Muscularis:1.25,Colon_Transverse:1.25):0.37,Thyroid:1.62):0.90):4.36):8.82,((((Artery_Coronary:1.04,Artery_Aorta:1.04):1.79,Adrenal_Gland:2.82):0.19,(Artery_Tibial:1.75,Adipose_Subcutaneous:1.75):1.27):1.19,(((((Pancreas:0.41,Adipose_Visceral_Omentum:0.41):0.22,Muscle_Skeletal:0.64):0.10,(Nerve_Tibial:0.51,Minor_Salivary_Gland:0.51):0.22):0.15,Ovary:0.88):0.91,Lung:1.79):2.41):11.48);";
+const tissue_tree = new Dendrogram(tissue_newick, orientation='v', dimentions={w:svgConfig.width - margin.left - margin.right, h: margin.top - 55});
+tissue_tree.draw(topg);
+
+
+// parse the heatmap
+const jsonFile = "genes.median.tpm.json";
+const useLog = true;
+
+d3.json(jsonFile, function(error, data){
+    data.forEach(function(d){
+        d.value = useLog?Math.log10(+d.medianTPM):+d.medianTPM;
+        d.x = d.tissueId;
+        d.y = d.geneSymbol;
+        d.originalValue = d.medianTPM;
+    });
+
+    // use d3.nest to group data
+    const dataByX = d3.nest()
+        .key(function(d){return d.x})
+        .entries(data);
+    const xList = dataByX.map((d) => d.key);
+    const dataByY = d3.nest()
+        .key((d) => d.y)
+        .entries(data);
+    const yList = dataByY.map((d) => d.key);
+
+    // set the scales
+    const colorScale = d3.scaleQuantile() // scaleQuantile maps the continuous domain to a discrete range
+        .domain([0, Math.round(d3.max(data, (d) => d.value))])
+        .range(svgConfig.colors);
+
+    const xScale = tissue_tree.xScale;
+
+    const yScale = gene_tree.yScale;
+
+    // color legend
+    const legend = legendg.selectAll(".legend")
+        .data([0].concat(colorScale.quantiles()), (d) => d);
+    const legendGroups = legend.enter().append("g")
+        .attr("class", "legend");
+    legendGroups.append("rect")
+        .attr("x", (d, i) => 40*i) // TODO: hard-coded value
+        .attr("y", 5)
+        .attr("width", 40) // TODO: hard-coded value
+        .attr("height", yScale.bandwidth()/2)
+        .style("fill", (d) => d==0?svgConfig.nullColor:colorScale(d));
+
+    legendGroups.append("text")
+        .attr("class", "normal")
+        .text((d) => "≥ " + d.toPrecision(2))
+        .attr("x", (d, i) => 40 * i) // TODO: hard-coded value
+        .attr("y", 15 + yScale.bandwidth()/2); // TODO: hard-coded value
+
+    // text labels
+    const xLabels = mapg.selectAll(".xLabel")
+        .data(xList)
+        .enter().append("text")
+        .text((d) => d.replace(/_/g, " "))
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("class", (d, i) => `xLabel normal x${i}`)
+        .style("text-anchor", "start")
+        .attr("transform", (d) => {
+            let angle = 60;
+            let x = xScale(d);
+            return `translate(${x}, ${yScale.range()[1] + (yScale.bandwidth()/2)}) rotate(${angle})`;
+        });
+
+    const yLabels = mapg.selectAll(".yLabel")
+        .data(yList)
+        .enter().append("text")
+        .text((d) => d)
+        .attr("x", xScale.range()[1] + (xScale.bandwidth()/2))
+        .attr("y", (d) => yScale(d) + (yScale.bandwidth()/1.5))
+        .attr("class", (d, i) => `yLabel normal y${i}`)
+        .style("text-anchor", "start");
+
+    // heatmap cells
+    const cells = mapg.selectAll(".cell")
+        .data(data, (d) => d.value);
+    cells.enter().append("rect")
+        .attr("row", (d) => `x${xList.indexOf(d.x)}`)
+        .attr("col", (d) => `y${yList.indexOf(d.y)}`)
+        .attr("x", (d) => xScale(d.x))
+        .attr("y", (d) => yScale(d.y))
+        .attr("rx", 2)
+        .attr('ry', 2)
+        .attr("class", (d) => `cell expressmap-bordered`)
+        .attr("width", xScale.bandwidth())
+        .attr("height", yScale.bandwidth())
+        .style("fill", (d) => svgConfig.colors[0])
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout)
+        .merge(cells)
+        .transition()
+        .duration(2000)
+        .style("fill", (d) => d.originalValue==0?svgConfig.nullColor:colorScale(d.value));
+
+});
 
 const tooltip = new Tooltip("tooltip");
 
@@ -72,88 +189,5 @@ const mouseout = function(d){
     selected.classed('expressmap-highlighted', false);
     tooltip.hide();
 };
-
-// parse the gene dendrogram
-const newick = "(((((SLC25A21:1.47,GAS6-AS1:1.47):1.53,SLC27A6:2.99):0.84,TMEM229A:3.83):2.34,((TMEM255B:2.47,TMEM106B:2.47):0.62,GAS6-AS2:3.08):3.09):7.25,(TMEM167B:4.53,GAS6:4.53):8.89);"
-var tree = new Dendrogram(newick);
-tree.draw(leftg);
-const jsonFile = "genes.median.tpm.json";
-const useLog = true;
-
-d3.json(jsonFile, function(error, data){
-    data.forEach(function(d){
-        d.value = useLog?Math.log10(+d.medianTPM):+d.medianTPM;
-        d.x = d.tissueId;
-        d.y = d.geneSymbol;
-        d.originalValue = d.medianTPM;
-    });
-
-    // use d3.nest to group by
-    const dataByX = d3.nest()
-        .key(function(d){return d.x})
-        .entries(data);
-    const xList = dataByX.map((d) => d.key);
-    const dataByY = d3.nest()
-        .key((d) => d.y)
-        .entries(data);
-    const yList = dataByY.map((d) => d.key);
-
-    // set the scales
-    const colorScale = d3.scaleQuantile() // scaleQuantile maps the continuous domain to a discrete range
-        .domain([0, svgConfig.colors.length-1, d3.max(data, (d) => d.value)])
-        .range(svgConfig.colors);
-
-    const xScale = d3.scaleBand()
-        .domain(xList)
-        .range([0, svgConfig.width - margin.left - margin.right])
-        .padding(.05);
-
-    const yScale = tree.yScale;
-
-    // text labels
-    const xLabels = mapg.selectAll(".xLabel")
-        .data(xList)
-        .enter().append("text")
-        .text((d) => d.replace(/_/g, " "))
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("class", (d, i) => `xLabel normal x${i}`)
-        .style("text-anchor", "start")
-        .attr("transform", (d) => {
-            let angle = 60;
-            let x = xScale(d);
-            return `translate(${x}, ${yScale.range()[1] + (yScale.bandwidth()/2)}) rotate(${angle})`;
-        });
-    const yLabels = mapg.selectAll(".yLabel")
-        .data(yList)
-        .enter().append("text")
-        .text((d) => d)
-        .attr("x", xScale.range()[1] + (xScale.bandwidth()/2))
-        .attr("y", (d) => yScale(d) + (yScale.bandwidth()/1.5))
-        .attr("class", (d, i) => `yLabel normal y${i}`)
-        .style("text-anchor", "start");
-
-    // heatmap cells
-    const cells = mapg.selectAll(".cell")
-        .data(data, (d) => d.value);
-    cells.enter().append("rect")
-        .attr("row", (d) => `x${xList.indexOf(d.x)}`)
-        .attr("col", (d) => `y${yList.indexOf(d.y)}`)
-        .attr("x", (d) => xScale(d.x))
-        .attr("y", (d) => yScale(d.y))
-        .attr("rx", 2)
-        .attr('ry', 2)
-        .attr("class", (d) => `cell expressmap-bordered`)
-        .attr("width", xScale.bandwidth())
-        .attr("height", yScale.bandwidth())
-        .style("fill", (d) => svgConfig.colors[0])
-        .on('mouseover', mouseover)
-        .on('mouseout', mouseout)
-        .merge(cells)
-        .transition()
-        .duration(2000)
-        .style("fill", (d) => d.originalValue==0?svgConfig.nullColor:colorScale(d.value));
-
-});
 
 
