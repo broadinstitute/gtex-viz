f3b4cd7f1b395634c2d1b06f762f1366a133c776
kataviz <khhuang@broadinstitute.org>
2018-03-09 12:42:51 -0500

    restructured API calls

--- src/scripts/main.junction.js
+++ src/scripts/main.junction.js
@@ -1,7 +1,7 @@
 import * as d4 from "d3";
 import DendroHeatmap from "./modules/DendroHeatmap";
 // import {getTissueClusters, getGeneClusters, getGtexUrls, parseTissues, parseMedianTPM, makeJsonForPlotly, parseMedianExpression} from "./modules/gtexDataParser";
-import {getGtexUrls, parseTissues, parseJunctionExpression} from "./modules/gtexDataParser";
+import {getGtexUrls, parseTissues, parseJunctionExpression, parseExons} from "./modules/gtexDataParser";
 import {downloadSvg} from "./modules/utils";
 
 const urls = getGtexUrls();
@@ -13,37 +13,42 @@ $(document).ready(function(){
         if(e.keyCode == 13){
             // bind the enter key
             e.preventDefault(); // or else, hitting the enter key's default is refreshing the page
-            searchJunctions();
+            init();
         }
     });
     $('#junctionSubmit').click(function(){
-        searchJunctions();
+        init();
     })
 });
 
-function searchJunctions(){
-    const input = $('#gene').val();
+function init(){
+    const inputGene = $('#gene').val();
     // const input = "SLK";
-    const domId = "chart";
     reset();
+    d4.json(urls.geneId + inputGene, function(json){  // get the gene object for the gencode ID
+        const gene = json.geneId[0];
+        process(gene.gencodeId);
+
+    });
+}
+
+function process(gencodeId){
+    const domId = "chart";
     $('#spinner').show();
     d4.queue()
         .defer(d4.json, urls.tissue) // get tissue colors
-        .defer(d4.json, urls.geneId + input) // get the gene object
-        .await(function(error, data1, data2){
-            const tissues = parseTissues(data1);
-            const gene = data2.geneId[0];
-            d4.json(urls.junctionExp + gene.gencodeId, function(juncJson){
-
-                const tissueTree = juncJson.clusters.tissue,
-                      junctionTree = juncJson.clusters.junction,
-                      expression = parseJunctionExpression(juncJson);
-                const dmap = new DendroHeatmap(junctionTree, tissueTree, expression, "reds2", 5);
-                dmap.render(domId, false, true); // false: no top tree, true: show left tree
-                $('#spinner').hide();
-            });
+        .defer(d4.json, urls.geneModel + gencodeId)
+        .defer(d4.json, urls.junctionExp + gencodeId)
+        .await(function(error, data1, data2, data3){
+            const tissues = parseTissues(data1),
+                exons = parseExons(data2),
+                tissueTree = data3.clusters.tissue,
+                junctionTree = data3.clusters.junction,
+                expression = parseJunctionExpression(data3);
+            const dmap = new DendroHeatmap(junctionTree, tissueTree, expression, "reds2", 5);
+            dmap.render(domId, false, true); // false: no top tree, true: show left tree
+            $('#spinner').hide();
         });
-
 }
 
 function reset(){
---
+++ src/scripts/modules/GeneModel.js
@@ -0,0 +1,7 @@
+import * as d4 from "d3"
+
+export default class GeneModel {
+    constructor (data){
+        this.data = data;
+    }
+}
