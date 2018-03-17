08743c4c8f4daf339e8e7344b68d66617f403fa9
kataviz <khhuang@broadinstitute.org>
2018-03-17 13:09:55 -0400

    re-wrote toolbar implementation

--- src/scripts/BatchGeneExpression.js
+++ src/scripts/BatchGeneExpression.js
@@ -2,14 +2,14 @@ import * as d4 from "d3";
 "use strict";
 import {getGtexUrls, parseTissues, parseMedianExpression, makeJsonForPlotly} from "./modules/gtex/gtexDataParser";
 import {colorChart} from "./modules/Colors";
+import {downloadSvg} from "./modules/utils";
 import DendroHeatmap from "./modules/DendroHeatmap";
-export function searchById(glist, domId, infoId){
-    // TODO: figure out what this line does...
-    d4.selectAll("*").classed("inView", false);
+
+export function searchById(glist, domId, infoboxId, toolbarId, urls = getGtexUrls()){
 
     if (d4.select(`#${domId}`).empty()) throw `Fatal Error: DOM element with id ${domId} does not exist;`;
-    const urls = getGtexUrls();
     let message = "";
+    let dmap;
     d4.queue()
     .defer(d4.json, urls.tissue) // get tissue colors
     .defer(d4.json, urls.geneId + glist.join(",")) // get gene objects
@@ -32,25 +32,25 @@ export function searchById(glist, domId, infoId){
                 const tissueTree = eData.clusters.tissue,
                       geneTree = eData.clusters.gene,
                       expression = parseMedianExpression(eData);
-                const dmap = new DendroHeatmap(tissueTree, geneTree, expression);
+                dmap = new DendroHeatmap(tissueTree, geneTree, expression);
                 dmap.render(domId);
-                customization(dmap, tissues);
+                customization(dmap, tissues, toolbarId);
                 
             });
         }
-        $(`#${infoId}`).html(message);
+        $(`#${infoboxId}`).html(message);
         $('#spinner').hide();
-
-
     });
+    return dmap;
 }
 
 /**
  * Customizes the GTEx gene expression DendroHeatmap
  * @param dmap {DendroHeatmap}
  * @param tissues [List] of GTEx tissue objects, with attr: tissue_id}
+ * @param toolbarId {String} the dom ID of the tool bar
  */
-function customization(dmap, tissues){
+function customization(dmap, tissues, toolbarId){
     const genes = dmap.data.heatmap;
     const tissueDict = tissues.reduce((a, d)=>{a[d.tissue_id] = d; return a;}, {});
     const geneDict = genes.reduce((a, d, i)=>{a[d.gencodeId]=d; return a;}, {});
@@ -66,8 +66,8 @@ function customization(dmap, tissues){
 
     addTissueColors(dmap, tissueDict);
     customizeHeatmapMouseEvents(dmap, tissueDict, geneDict);
-    //
-    // bindToolbarEvents(dmap, tissueDict, geneDict);
+    createToolbar(toolbarId, dmap, tissueDict);
+    bindToolbarEvents(dmap, tissueDict, geneDict);
 
 }
 
@@ -244,5 +244,142 @@ function renderBoxplot(action, gene, geneDict, tissueDict, dmap) {
 
 }
 
+/**
+ * create the toolbar
+ * @param domId {String} the toolbar DOM ID
+ */
+function createToolbar(domId, dmap, tissueDict){
+    // fontawesome reference: http://fontawesome.io/examples/
+    // jQuery syntax
+    $(`#${domId}`).show();
 
+    let $barDiv = $("<div/>").addClass("btn-group btn-group-sm").appendTo(`#${domId}`);
+
+    const id1 = "sortTissues";
+    let $button1 = $("<a/>").attr("id", id1)
+        .attr("href", `#${id1}`)
+        .addClass("btn btn-default").appendTo($barDiv);
+    $("<i/>").addClass("fa fa-sort-alpha-down").appendTo($button1); // a fontawesome icon
+
+    d4.select(`#${id1}`)
+        .on("click", function(){
+            // hides the tissue dendrogram
+            d4.select("#" + dmap.config.panels.top.id).style("display", "None");
+            // sort tissues
+            let xlist = dmap.objects.heatmap.xList.sort();
+            sortTissues(xlist, dmap, tissueDict);
+        })
+        .on("mouseover", function(){
+            dmap.visualComponents.tooltip.show("Sort Tissues Alphabetically");
+        })
+        .on("mouseout", function(){
+            dmap.visualComponents.tooltip.hide();
+        });
+
+    const id2 = "clusterTissues";
+    let $button2 = $("<a/>").attr("id", id2)
+        .attr("href", `#${id2}`)
+        .addClass("btn btn-default").appendTo($barDiv);
+    $("<i/>").addClass("fa fa-code-branch").appendTo($button2);
+
+    d4.select(`#${id2}`)
+        .on("click", function(){
+            d4.select("#" + dmap.config.panels.top.id).style("display", "Block");  // shows the tissue dendrogram
+            let xlist = dmap.objects.columnTree.xScale.domain();
+            sortTissues(xlist, dmap, tissueDict);
+        })
+        .on("mouseover", function(){
+            dmap.visualComponents.tooltip.show("Cluster Tissues");
+        })
+        .on("mouseout", function(){
+            dmap.visualComponents.tooltip.hide();
+        });
+
+    const id3 = "expMapDownload";
+    let $button3 = $("<a/>").attr("id", id3)
+        .attr("href", `#${id3}`)
+        .addClass("btn btn-default").appendTo($barDiv);
+    $("<i/>").addClass("fa fa-save").appendTo($button3);
+
+    d4.select(`#${id3}`)
+        .on("click", function(){
+            let svgObj = $($($(`${"#" +dmap.config.id} svg`))[0]); // jQuery dependent
+            downloadSvg(svgObj, "heatmap.svg", "downloadTempDiv");
+        })
+        .on("mouseover", function(){
+            dmap.visualComponents.tooltip.show("Download Heatmap");
+        })
+        .on("mouseout", function(){
+            dmap.visualComponents.tooltip.hide();
+        });
+
+}
+
+/////// customized toolbar events ///////
+/**
+ *
+ * @param dmap {DendroHeatmap}
+ * @param tissueDict {Dictionary} of tissue objects indexed by tissue ID
+ */
+// function bindToolbarEvents(dmap, tissueDict){
+//     dmap.toolBar = {};
+
+    // d4.select("#sortTissues")
+    //     .on("click", function(){
+    //         sortTissuesByAlphabet(dmap, tissueDict);
+    //     })
+    //     .on("mouseover", function(){
+    //         dmap.visualComponents.tooltip.show("Sort Tissues Alphabetically");
+    //     })
+    //     .on("mouseout", function(){
+    //         dmap.visualComponents.tooltip.hide();
+    //     });
+    //
+    // d4.select("#sortTissuesByClusters")
+    //     .on("click", function(){
+    //         d4.select("#" + dmap.config.panels.top.id)
+    //             .style("display", "Block");  // shows the tissue dendrogram
+    //         let xlist = dmap.objects.columnTree.xScale.domain();
+    //         sortTissueClickHelper(xlist, dmap, tissueDict);
+    //     })
+    //     .on("mouseover", function(){
+    //         dmap.visualComponents.tooltip.show("Cluster Tissues");
+    //     })
+    //     .on("mouseout", function(){
+    //         dmap.visualComponents.tooltip.hide();
+    //     });
+
+//     d4.select("#downloadHeatmap")
+//         .on("click", function(){
+//             let svgObj = $($($(`${"#" +dmap.config.id} svg`))[0]); // jQuery dependent
+//             downloadSvg(svgObj, "heatmap.svg", "downloadTempDiv");
+//         })
+//         .on("mouseover", function(){
+//             dmap.visualComponents.tooltip.show("Download Heatmap");
+//         })
+//         .on("mouseout", function(){
+//             dmap.visualComponents.tooltip.hide();
+//         });
+// }
+
+function sortTissues (xlist, dmap, tissueDict){
+
+    // updates the heatmap
+    const dom = d4.select("#"+dmap.config.panels.main.id);
+    const dimensions = dmap.config.panels.main;
+    dmap.objects.heatmap.redraw(dom, xlist, dmap.objects.heatmap.yList, dimensions);
+
+    // changes the tissue display text to tissue names
+    d4.selectAll(".exp-map-xlabel")
+        .text((d) => tissueDict[d]===undefined?d:tissueDict[d].tissue_name);
+    addTissueColors(dmap, tissueDict);
+
+    // hides the boxplot
+    d4.select('#boxplot').style("opacity", 0.0);
+
+    // deselects genes
+    d4.selectAll(".exp-map-ylabel").classed("clicked", false);
+    dmap.data.external = {};
+
+}
 
