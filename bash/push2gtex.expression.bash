786c0eb43f20d2849f8e43d0ef32b337ec1e3824
kataviz <khhuang@broadinstitute.org>
2018-05-19 10:30:43 -0400

    reorganized bash files

--- bash/push2gtex.expression.bash
+++ bash/push2gtex.expression.bash
@@ -3,8 +3,8 @@ SOURCE=$HOME/gtex-d3
 DEST=$HOME/gtex-cloud/contexts/external/gtex/media/expressHeatmap
 
 export NODE_ENV="prod"
-rollup -c rollup.batch.gene.expression.config.js
-rollup -c rollup.isoform.expression.config.js
+rollup -c rollup/rollup.batch.gene.expression.config.js
+rollup -c rollup/rollup.isoform.expression.config.js
 
 echo "Copying *.min.js and *.css to $DEST"
 cp $SOURCE/build/js/batch-gene-expression.bundle.min.js $DEST/build
