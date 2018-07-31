9189434041ba752dc9dd6c2845276138e90c7f93
Kane Hadley <khadley@broadinstitute.org>
2018-07-31 17:38:57 -0400

    Updating during rationalization.

--- bash/push2gtex.eqtlDashboard.bash
+++ bash/push2gtex.eqtlDashboard.bash
@@ -1,9 +1,11 @@
 HOME=/Users/khhuang/Sites
+HOME=/var/www/html
 SOURCE=$HOME/gtex-d3
 DEST=$HOME/gtex-cloud/contexts/external/gtex/media/eqtlDashboard
 
 export NODE_ENV="prod"
-rollup -c $SOURCE/rollup/rollup.eqtl.dashboard.config.js
+#rollup -c $SOURCE/rollup/rollup.eqtl.dashboard.config.js
+rollup -c rollup/rollup.eqtl.dashboard.config.js
 
 echo "Copying *.min.js and *.css to $DEST"
 cp $SOURCE/build/js/eqtl-dashboard.bundle.min.js $DEST/build
