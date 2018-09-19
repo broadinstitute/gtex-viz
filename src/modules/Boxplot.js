d32669b550f24f95ed00c5ecff765c94842afd2f
dnguyen <dnguyen@broadinstitute.org>
2018-09-19 10:52:07 -0400

    rendering x-axis

--- src/modules/Boxplot.js
+++ src/modules/Boxplot.js
@@ -5,20 +5,33 @@
 
 import {ascending, quantile, max, min} from 'd3-array';
 import {select} from 'd3-selection';
+import {scaleOrdinal} from 'd3-scale';
+import {axisBottom} from 'd3-axis';
 
 export default class Boxplot {
     constructor(boxplotData){
         this.boxplotData = boxplotData;
+        this.allVals = [];
+        boxplotData.forEach(d => this.allVals = this.allVals.concat(d.data));
+        this.allVals.sort();
     }
 
     render(rootId) {
-        const svg = this._createSvg(rootId);
+        // TODO: make width/height and margins customizable
+        let width = 800;
+        let height = 600;
+        let marginLR = 25;
+        const svg = this._createSvg(rootId, width, height);
+        let xScale = scaleOrdinal().domain([0, this.boxplotData.length]).range([marginLR, width - marginLR]);
+        let xAxis = axisBottom().scale(xScale);
+        svg.append('g').call(xAxis);
     }
 
-    _createSvg(rootId) {
+    _createSvg(rootId, width=800, height=600) {
+        // TODO: make width/height customizable
         let svg = select(`#${rootId}`).append('svg')
-            .attr('width', 800)
-            .attr('height', 600);
+            .attr('width', width)
+            .attr('height', height);
         return svg;
     }
 }
