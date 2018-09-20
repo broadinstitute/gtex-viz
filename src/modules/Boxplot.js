f457b969328ee40dbefc6fcb035348ae0307629f
dnguyen <dnguyen@broadinstitute.org>
2018-09-19 22:17:51 -0400

    displaying x-axis and y-axis for linear scale

--- src/modules/Boxplot.js
+++ src/modules/Boxplot.js
@@ -3,10 +3,10 @@
  * Licensed under the BSD 3-clause license (https://github.com/broadinstitute/gtex-viz/blob/master/LICENSE.md)
  */
 
-import {ascending, quantile, max, min} from 'd3-array';
+import {ascending, quantile, extent} from 'd3-array';
 import {select} from 'd3-selection';
-import {scaleOrdinal} from 'd3-scale';
-import {axisBottom} from 'd3-axis';
+import {scaleOrdinal, scaleLinear} from 'd3-scale';
+import {axisBottom, axisLeft} from 'd3-axis';
 
 export default class Boxplot {
     constructor(boxplotData){
@@ -20,11 +20,14 @@ export default class Boxplot {
         // TODO: make width/height and margins customizable
         let width = 800;
         let height = 600;
-        let marginLR = 25;
+        let marginLR = 40;
+        let marginTB = 25;
         const svg = this._createSvg(rootId, width, height);
-        let xScale = scaleOrdinal().domain([0, this.boxplotData.length]).range([marginLR, width - marginLR]);
-        let xAxis = axisBottom().scale(xScale);
-        svg.append('g').call(xAxis);
+        const dom = svg.append('g').attr('id', 'gtex-viz-boxplot');
+        let scales = this._setScales(width, height, marginLR, marginTB);
+
+        dom.append('g').attr('transform', `translate(${marginLR}, ${height - marginTB})`).call(scales.x);
+        dom.append('g').attr('transform', `translate(${marginLR}, ${marginTB})`).call(scales.y);
     }
 
     _createSvg(rootId, width=800, height=600) {
@@ -33,5 +36,22 @@ export default class Boxplot {
             .attr('width', width)
             .attr('height', height);
         return svg;
+    }
+
+    _setScales(width, height, marginLR, marginTB) {
+        let xScale = scaleOrdinal()
+            .domain([0, this.boxplotData.length])
+            .range([0, width - (marginLR * 2)]);
+        let xAxis = axisBottom().scale(xScale);
+
+        let yScale = scaleLinear()
+            .domain(extent(this.allVals))
+            .range([height - (2 * marginTB), 0]);
+        let yAxis = axisLeft().scale(yScale);
+
+        return {
+            x: xAxis,
+            y: yAxis
+        };
     }
 }
