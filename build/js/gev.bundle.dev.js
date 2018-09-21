ffd5b441429f7711431c7ec18d1dc0a1a96a6b12
kataviz <khhuang@broadinstitute.org>
2018-09-21 14:10:05 -0400

    WIP: started rewriting GEV

---
+++ build/js/gev.bundle.dev.js
@@ -0,0 +1,19 @@
+var GeneEqtlVisualizer = (function (exports) {
+'use strict';
+
+/**
+ * Copyright © 2015 - 2018 The Broad Institute, Inc. All rights reserved.
+ * Licensed under the BSD 3-clause license (https://github.com/broadinstitute/gtex-viz/blob/master/LICENSE.md)
+ */
+"use strict";
+
+function render(geneId, rootDiv){
+    console.log(geneId);
+}
+
+exports.render = render;
+
+return exports;
+
+}({}));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2V2LmJ1bmRsZS5kZXYuanMiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9HZW5lRXF0bFZpc3VhbGl6ZXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBDb3B5cmlnaHQgwqkgMjAxNSAtIDIwMTggVGhlIEJyb2FkIEluc3RpdHV0ZSwgSW5jLiBBbGwgcmlnaHRzIHJlc2VydmVkLlxuICogTGljZW5zZWQgdW5kZXIgdGhlIEJTRCAzLWNsYXVzZSBsaWNlbnNlIChodHRwczovL2dpdGh1Yi5jb20vYnJvYWRpbnN0aXR1dGUvZ3RleC12aXovYmxvYi9tYXN0ZXIvTElDRU5TRS5tZClcbiAqL1xuXCJ1c2Ugc3RyaWN0XCI7XG5cbmV4cG9ydCBmdW5jdGlvbiByZW5kZXIoZ2VuZUlkLCByb290RGl2KXtcbiAgICBjb25zb2xlLmxvZyhnZW5lSWQpO1xufSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQTs7OztBQUlBLFlBQVksQ0FBQzs7QUFFYixBQUFPLFNBQVMsTUFBTSxDQUFDLE1BQU0sRUFBRSxPQUFPLENBQUM7SUFDbkMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxNQUFNLENBQUMsQ0FBQzs7Ozs7Ozs7Ozs7In0=
