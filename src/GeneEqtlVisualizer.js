21bcc9b1a20480f8ac775f86ff7701548ae20485
kataviz <khhuang@broadinstitute.org>
2018-09-24 18:58:34 -0400

    WIP: creating BubbleMap.js

--- src/GeneEqtlVisualizer.js
+++ src/GeneEqtlVisualizer.js
@@ -3,7 +3,60 @@
  * Licensed under the BSD 3-clause license (https://github.com/broadinstitute/gtex-viz/blob/master/LICENSE.md)
  */
 "use strict";
+import {json} from "d3-fetch";
+import {checkDomId, createSvg} from "./modules/utils";
+import {
+    getGtexUrls,
+    parseGenes,
+    parseSingleTissueEqtls
+} from "./modules/gtexDataParser";
+import BubbleMap from "./modules/BubbleMap";
 
-export function render(geneId, rootDiv){
+export function render(geneId, rootDivId, spinnerId, urls = getGtexUrls()){
     console.log(geneId);
+    json(urls.geneId + geneId) // query the gene by geneId which could be gene name or gencode ID with or withour versioning
+        .then(function(data){
+            let gene = parseGenes(data, true, geneId); // fetch the gene by user specified gene ID
+            json(urls.singleTissueEqtl + gene.gencodeId)
+                .then(function(data2){
+                    let eqtls = parseSingleTissueEqtls(data2);
+                    let gevConfig = {
+                        id: rootDivId,
+                        data: eqtls,
+                        width: 2500, //window.innerWidth*0.9,
+                        height: window.innerWidth/3, // TODO: use a dynamic width based on the matrix size
+                        marginTop: 50,
+                        marginRight: 100,
+                        marginBottom: 30,
+                        marginLeft: 30,
+                        rowLabelWidth: 100,
+                        bottomLabelHeight: 100,
+                        showLabels: true,
+                        columnLabelAngle: 30,
+                        columnLabelPosAdjust: 10,
+                        useLog: false,
+                        logBase: 10,
+                        colorScheme: "RdBu", // a diverging color scheme
+                        colorDomain: [-1, 1]
+                    };
+                    renderBubbleMap(gevConfig);
+                    $('#' + spinnerId).hide();
+                })
+        })
+}
+
+export function renderBubbleMap(par){
+    let margin = {
+        left: par.marginLeft + par.rowLabelWidth,
+        top: par.marginTop,
+        right: par.marginRight,
+        bottom: par.marginBottom
+    };
+    let inWidth = par.width - (par.rowLabelWidth + par.marginLeft + par.marginRight);
+    let inHeight = par.height - (par.bottomLabelHeight + par.marginTop + par.marginBottom);
+    let svg = createSvg(par.id, par.width, par.height, margin);
+    console.log(par.data);
+    let bmap = new BubbleMap(par.data, par.useLog, par.logBase, par.colorScheme, par.id+"-tooltip");
+    bmap.draw(svg, {w:inWidth, h:inHeight}, par.columnLabelAngle, par.columnLabelPosAdjust, par.colorDomain, par.showLabels);
+    return bmap;
 }
---
+++ src/modules/BubbleMap.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright © 2015 - 2018 The Broad Institute, Inc. All rights reserved.
+ * Licensed under the BSD 3-clause license (https://github.com/broadinstitute/gtex-viz/blob/master/LICENSE.md)
+ */
+
+"use strict";
+import {nest} from "d3-collection";
+import {extent} from "d3-array";
+import {select, selectAll} from "d3-selection";
+import {scaleBand, scaleLinear} from "d3-scale";
+import Tooltip from "./Tooltip";
+import {setColorScale} from "./colors";
+
+export default class BubbleMap {
+    constructor(data, useLog=true, logBase=10, colorScheme="Reds", tooltipId = "tooltip"){
+        this.data = data;
+        this.useLog = useLog;
+        this.logBase = logBase;
+        this.colorScheme = colorScheme;
+
+        // initiates additional attributes
+        // this.xList = undefined;
+        // this.yList = undefined;
+        this.xScale = undefined;
+        this.yScale = undefined;
+        this.colorScale = undefined;
+        this.bubbleScale = undefined;
+
+        // peripheral features
+        // Tooltip
+        if ($(`#${tooltipId}`).length == 0) $('<div/>').attr('id', tooltipId).appendTo($('body'));
+        this.tooltip = new Tooltip(tooltipId);
+        select(`#${tooltipId}`).classed('bubblemap-tooltip', true);
+
+        // Toolbar
+        this.toolbar = undefined;
+    }
+
+    draw(dom, dimensions={w:1000, h:600}, columnLabelAngle=30, columnLabelPosAdjust=undefined, colorDomain=undefined, showLabels=true){
+        if (this.xScale === undefined) this._setXScale(dimensions.w);
+        if (this.yScale === undefined) this._setYScale(dimensions.h);
+        if (this.colorScale === undefined) this._setColorScale(colorDomain);
+        if (this.bubbleScale === undefined) this._setBubbleScale();
+
+        // text labels
+        if(showLabels){
+            // column labels
+            let xLabels = dom.selectAll().data(this.xScale.domain())
+                .enter().append("text")
+                .attr("class", (d, i)=>`bubble-map-xlabel x${i}`)
+                .attr("x", 0)
+                .attr("y", 0)
+                .style("text-anchor", "start")
+                .style("cursor", "default")
+                .attr("transform", (d) => {
+                    let x = this.xScale(d) + 5; // TODO: remove hard-coded value
+                    let y = columnLabelPosAdjust===undefined?this.yScale.range()[1]:this.yScale.range()[1]+columnLabelPosAdjust;
+                    return `translate(${x}, ${y}) rotate(${columnLabelAngle})`;
+                })
+                .text((d)=>d);
+
+            // row labels
+            let yLabels = dom.selectAll().data(this.yScale.domain())
+                .enter().append("text")
+                .attr("class", (d, i) => `bubble-map-ylabel y${i}`)
+                .attr("x", this.xScale.range()[0] + 5)
+                .attr("y", (d) => this.yScale(d) + 10)
+                .style("text-anchor", "end")
+                .style("cursor", "default")
+                .text((d)=>d);
+        }
+    }
+
+    // private methods
+    _setXScale(width){
+        // use d3 nest data structure to find the unique list of x labels
+        // reference: https://github.com/d3/d3-collection#nests
+        let xList = nest()
+            .key((d) => d.x) // group this.data by d.x
+            .entries(this.data)
+            .map((d) => d.key) // then return the unique list of d.x
+            .sort((a, b) => {return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;});
+        console.log(xList);
+        this.xScale = scaleBand() // reference: https://github.com/d3/d3-scale#scaleBand
+            .domain(xList) // perhaps it isn't necessary to store xList, it could be retrieved by xScale.domain
+            .range([0, width])
+            .padding(.05); // temporarily hard-coded value
+    }
+
+    _setYScale(height){
+        // use d3 nest data structure to find the unique list of y labels
+        // reference: https://github.com/d3/d3-collection#nests
+        let yList = nest()
+            .key((d) => d.y) // group this.data by d.x
+            .entries(this.data)
+            .map((d) => d.key) // then return the unique list of d.x
+            .sort((a, b) => {return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;});
+        console.log(yList);
+        this.yScale = scaleBand() // reference: https://github.com/d3/d3-scale#scaleBand
+            .domain(yList) // perhaps it isn't necessary to store xList, it could be retrieved by xScale.domain
+            .range([0, height])
+            .padding(.05); // temporarily hard-coded value
+    }
+
+    _setColorScale(domain){
+        let useLog = this.useLog;
+        let data = domain===undefined?this.data.map((d)=>useLog?this._log(d.value):d.value):domain;
+        this.colorScale = setColorScale(data, this.colorScheme, undefined, undefined, true);
+    }
+
+    _setBubbleScale(range={max:30, min:1}){
+        this.bubbleScale = scaleLinear()
+            .domain(extent(this.data.map((d)=>d.r)))
+            .range([range.min, range.max]);
+
+    }
+
+    _log(v){
+        const adjust = 1;
+        return Math.log(Number(v+adjust))/Math.log(this.logBase);
+    }
+
+
+}
+
