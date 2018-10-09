9861bc64487c4fe2079ae19e3150d0c32e238d1c
dnguyen <dnguyen@broadinstitute.org>
2018-10-09 17:21:12 -0400

    displaying data sorted by groups (i.e. tissue) for default display

--- src/GeneExpressionViolinPlot.js
+++ src/GeneExpressionViolinPlot.js
@@ -36,6 +36,7 @@ export function launch(rootId, tooltipRootId, gencodeId, urls=getGtexUrls()) {
                 .append('g')
                     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+
     Promise.all(promises)
         .then(function(args) {
             const tissues = parseTissues(args[0]);
@@ -45,11 +46,28 @@ export function launch(rootId, tooltipRootId, gencodeId, urls=getGtexUrls()) {
                 tissueIdNameMap[x.tissueSiteDetailId] = x.tissueSiteDetail;
                 tissueIdColorMap[x.tissueSiteDetailId] = x.colorHex;
             });
-            const violinPlotData = parseGeneExpressionForViolin(args[1]);
+            const violinPlotData = parseGeneExpressionForViolin(args[1]).sort(function(a, b) {
+                if (a.group < b.group) return -1;
+                else if (a.group > b.group) return 1;
+                else return 0;
+            });
             console.warn(violinPlotData);
             let violinPlot = new GroupedViolin(violinPlotData);
             violinPlot.createTooltip(ids.tooltipId)
-            violinPlot.render(svg, dim.width, dim.height);
+// render(dom, width=500, height=357, xPadding=0.05, xDomain=undefined, yDomain=[-3,3], yLabel="Y axis",
+//    showX=true, showSubX=true, subXAngle=0,
+//    showWhisker=false, showDivider=false, showLegend=false, showSize=false)
+            let width = dim.width;
+            let height = dim.height;
+            let xPadding = 0.1;
+            // let xDomain = undefined;
+            // let yDomain =[-3,3];
+            // let yLabel = 'Y axis';
+            // let showWhisker = false;
+            // let showDivider = false;
+            // let showLegend = false;
+            // let showSize = false;
+            violinPlot.render(svg, width, height, xPadding);
         });
 }
 
