efba0b0184ef3e8272527de78e89f5a2ca403a07
dnguyen <dnguyen@broadinstitute.org>
2018-12-11 17:54:28 -0500

    removing dev bundled version and cleaning up rollup config

--- rollup/rollup.gev.config.js
+++ rollup/rollup.gev.config.js
@@ -1,13 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
-// import replace from 'rollup-plugin-replace';
 import {minify} from 'uglify-es';
 
-/* to set the NODE_ENV
-in a terminal window (bash)
-export NODE_ENV="development"
-echo $NODE_ENV
- */
 const name= 'GeneEqtlVisualizer';
 export default {
     input: 'src/' + name + '.js',
@@ -19,6 +13,6 @@ export default {
     name: name,
     plugins: [
         nodeResolve({jsnext: true, main: true}),
-        uglify({}, minify) // uglify for production: NODE_ENV=production rollup -c
+        uglify({}, minify)
     ]
 }
